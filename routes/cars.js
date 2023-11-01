c616b93dc03925591f13e2b5264bd31c5fddf7fe
Chamanthi Aluri <143573994+alurichamanthi@users.noreply.github.com>
2023-11-01 14:56:20 -0500

    cars

--- routes/cars.js
+++ routes/cars.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('cars', { title: 'Search Results - Cars' });
+  res.render('cars', { title: 'Search Results shoes' });
 });
 
 module.exports = router;
