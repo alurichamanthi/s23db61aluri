a9fc9ed6649f51a424d8b8461e8af9d5836317a0
Chamanthi Aluri <143573994+alurichamanthi@users.noreply.github.com>
2023-11-15 22:41:57 -0600

    changes

--- controllers/animals.js
+++ controllers/animals.js
@@ -88,6 +88,69 @@ exports.animals_update_put = async function(req, res) {
     failed`);
     }
     };
-    
-    
+    // Handle Costume delete on DELETE.
+exports.animals_delete = async function(req, res) {
+ console.log("delete " + req.params.id)
+ try {
+ result = await animals.findByIdAndDelete( req.params.id)
+ console.log("Removed " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": Error deleting ${err}}`);
+ }
+};
+// Handle a show one view with id specified by query
+exports.animals_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await animals.findById( req.query.id)
+    res.render('animalsdetail',
+   { title: 'animals Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };   
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.animals_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('animalscreate', { title: 'animals Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.animals_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await animals.findById(req.query.id)
+    res.render('animalsupdate', { title: 'animals Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle a delete one view with id from query
+exports.animals_delete_Page = async function(req, res) {
+ console.log("Delete view for id " + req.query.id)
+ try{
+ result = await animals.findById(req.query.id)
+ res.render('animalsdelete', { title: 'animals Delete', toShow:
+result });
+ }
+ catch(err){
+ res.status(500)
+ res.send(`{'error': '${err}'}`);
+ }
+};
 
--- routes/animals.js
+++ routes/animals.js
@@ -3,4 +3,12 @@ const animals_controlers= require('../controllers/animals');
 var router = express.Router();
 /* GET costumes */
 router.get('/', animals_controlers.animals_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', animals_controlers.animals_view_one_Page);
+/* GET create costume page */
+router.get('/create', animals_controlers.animals_create_Page);
+/* GET create update page */
+router.get('/update', animals_controlers.animals_update_Page);
+/* GET delete costume page */
+router.get('/delete', animals_controlers.animals_delete_Page);
 module.exports = router;
